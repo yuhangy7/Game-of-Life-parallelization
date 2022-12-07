90ffc0e8479f2559a723a51ff871b7ed143c75b0
elvafan <40036572+elvafan@users.noreply.github.com>
2022-12-06 21:59:03 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -48,6 +48,15 @@ for num in {10000..31000..3000}
 do
 ./gol "$num" inputs/1k.pbm outputs/1k.pbm -v4 -t output1c2.txt
 done
+#cuda 3
+for num in {1000..9000..1000}
+do
+./gol "$num" inputs/1k.pbm outputs/1k.pbm -v5 -t output1c3.txt
+done
+for num in {10000..31000..3000}
+do
+./gol "$num" inputs/1k.pbm outputs/1k.pbm -v5 -t output1c3.txt
+done
 
 #matrix size 
 #run sequential 
@@ -103,7 +112,18 @@ num=$((num * 2))
 done
 for num in {1..10}
 do
-./gol 1000 inputs/"$num"k.pbm outputs/"$num"k.pbm -v4 -t output2c3.txt
+./gol 1000 inputs/"$num"k.pbm outputs/"$num"k.pbm -v4 -t output2c2.txt
+done
+#cuda 3
+num=32
+until [ $num -gt 1000 ]
+do
+./gol 1000 inputs/"$num".pbm outputs/"$num".pbm -v5 -t output2c3.txt
+num=$((num * 2))
+done
+for num in {1..10}
+do
+./gol 1000 inputs/"$num"k.pbm outputs/"$num"k.pbm -v5 -t output2c3.txt
 done
 
 #openmp number of thread
