df6de0aa23fb42fee02f44f0264c574ceb6b4927
elvafan <40036572+elvafan@users.noreply.github.com>
2022-12-09 14:53:37 -0500

    Update runopenmp.sh

--- runopenmp.sh
+++ runopenmp.sh
@@ -4,7 +4,7 @@ until [ $num -gt 1000 ]
 do
 for x in {1..24}
 do 
-./gol 1inputs/"$num".pbm outputs/"$num".pbm -v1 -n"$x" -t output5h.txt
+./gol 800 1inputs/"$num".pbm outputs/"$num".pbm -v1 -n"$x" -t output5h.txt
 done
 echo "XXXXXXXXXXXXXXX\n" > output5h.txt
 num=$((num * 2))
@@ -14,7 +14,7 @@ for num in {1..10}
 do
 for x in {1..24}
 do 
-./gol 1inputs/"$num"k.pbm outputs/"$num"k.pbm -v1 -n"$x" -t output5h.txt
+./gol 800 1inputs/"$num"k.pbm outputs/"$num"k.pbm -v1 -n"$x" -t output5h.txt
 done
 echo "XXXXXXXXXXXXXXX\n" > output5h.txt
 done
@@ -24,7 +24,7 @@ until [ $num -gt 1000 ]
 do
 for x in {1..24}
 do 
-./gol 1inputs/"$num".pbm outputs/"$num".pbm -v2 -n"$x" -t output5v.txt
+./gol 800 1inputs/"$num".pbm outputs/"$num".pbm -v2 -n"$x" -t output5v.txt
 done
 echo "XXXXXXXXXXXXXXX\n" > output5v.txt
 num=$((num * 2))
@@ -34,7 +34,7 @@ for num in {1..10}
 do
 for x in {1..24}
 do 
-./gol 1inputs/"$num"k.pbm outputs/"$num"k.pbm -v2 -n"$x" -t output5v.txt
+./gol 800 1inputs/"$num"k.pbm outputs/"$num"k.pbm -v2 -n"$x" -t output5v.txt
 done
 echo "XXXXXXXXXXXXXXX\n" > output5v.txt
 done
