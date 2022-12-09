a1be37413bfc904c1989dfa4490d7eec6a7a02e9
elvafan <40036572+elvafan@users.noreply.github.com>
2022-12-09 15:08:04 -0500

    Update runopenmp.sh

--- runopenmp.sh
+++ runopenmp.sh
@@ -1,16 +1,6 @@
 #openmp horinzontal
-num=32
-until [ $num -gt 1000 ]
-do
-for x in {1..24}
-do 
-./gol 800 inputs/"$num".pbm outputs/"$num".pbm -v1 -n"$x" -t output5h.txt
-done
-echo "XXXXXXXXXXXXXXX\n" > output5h.txt
-num=$((num * 2))
-done
-echo "XXXXXXXXXXXXXXX\n" > output5h.txt
-for num in {1..10}
+./gol 800 inputs/512.pbm outputs/512.pbm -v1 -n"$x" -t output5h.txt
+for num in {1..16..2}
 do
 for x in {1..24}
 do 
@@ -19,22 +9,12 @@ done
 echo "XXXXXXXXXXXXXXX\n" > output5h.txt
 done
 #openmp vertical
-num=32
-until [ $num -gt 1000 ]
+./gol 800 inputs/512.pbm outputs/512.pbm -v2 -n"$x" -t output5h.txt
+for num in {1..16..2}
 do
 for x in {1..24}
 do 
-./gol 800 inputs/"$num".pbm outputs/"$num".pbm -v2 -n"$x" -t output5v.txt
+./gol 800 inputs/"$num"k.pbm outputs/"$num"k.pbm -v2 -n"$x" -t output5h.txt
 done
-echo "XXXXXXXXXXXXXXX\n" > output5v.txt
-num=$((num * 2))
-done
-echo "XXXXXXXXXXXXXXX\n" > output5v.txt
-for num in {1..10}
-do
-for x in {1..24}
-do 
-./gol 800 inputs/"$num"k.pbm outputs/"$num"k.pbm -v2 -n"$x" -t output5v.txt
-done
-echo "XXXXXXXXXXXXXXX\n" > output5v.txt
+echo "XXXXXXXXXXXXXXX\n" > output5h.txt
 done
